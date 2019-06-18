8d20315adc30c15d7a0d63daeb32087540b4755c
emilyc7 <emilyc7@vt.edu>
2019-06-18 11:26:08 -0400

    fix bug with removing last letter of some company names

--- fortune500cleaner/company_editor.py
+++ fortune500cleaner/company_editor.py
@@ -1,17 +1,13 @@
-import numpy as np
-import torch
-import pandas
-
 
 def read_txt(file_name, new_file_name):
     f = open(file_name, "r")
     fh = open(new_file_name, "w")
     f1 = f.readlines()
     for x in f1:
-        while ord(x[0]) < 58:
+        while ord(x[0]) < 65:
             x = x[1:]
         while ord(x[len(x)-1]) < 65:
-            x = x[:len(x)-2]
+            x = x[:len(x)-1]
         print(x)
         x = x + "\n"
         fh.writelines(x)
