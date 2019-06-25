f0c12da9c68c4603ec98517521bb229c78c92e6a
AlfredJoseph <ajosep16@uncc.edu>
2019-06-25 15:27:57 -0400

    Update

--- Web2String.py
+++ Web2String.py
@@ -5,16 +5,19 @@ from bs4 import BeautifulSoup
 #import company_identifier
 
 # downloads url and converts to a HTML
-webURL = requests.get('https://finance.yahoo.com/news/deutsche-bank-db-continues-overhauling-113311379.html')
+webURL = requests.get('https://www.forbes.com/sites/kenkam/2019/06/25/tesla-beating-competitors-even-with-its-hands-tied/#72f8c1041d8f')
 soup = BeautifulSoup(webURL.content, 'html.parser')
 
-# Gets only the text in string form under <body> tag of HTML
-x = soup.find_all('p')[0].get_text()
+# Loops through all the instances with a <p> tag and prints them
+y = soup.find_all('p')
+z = 0
+for x in y:
+    a = soup.find_all('p')[z].get_text()
+    print(a)
+    z += 1
 
 
-# Just to test that it works
-# company_identifier.entity(x)
-print(x)
+
 
 
 
