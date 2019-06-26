26a1305026437c70e4f40efd6270bce39d47642a
AlfredJoseph <ajosep16@uncc.edu>
2019-06-26 16:08:21 -0400

    Crawls the web and outputs top 5 links to websites about entity/topic

---
+++ StockTest.py
@@ -0,0 +1,25 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas_datareader.data as web
+from company_identifier import find_ticker as ft
+
+
+style.use('ggplot')
+# Sets the start and end date for stock data you need
+start = dt.datetime(2019, 1, 1)
+end = dt.datetime.now()
+
+# uses find_ticker from company_identifier
+compName = "Apple"
+tickName = ft(compName)
+
+# Gets the stock data from Yahoo API
+df = web.DataReader(tickName, "yahoo", start, end)
+
+# formats the data better
+df.reset_index(inplace=True)
+df.set_index("Date", inplace=True)
+
+df['Adj Close'].plot()
+plt.show()
