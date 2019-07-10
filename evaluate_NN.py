bffd87fbc17e3e8018a213bbb33c6d00010495ab
steveZak <chesl97@gmail.com>
2019-07-10 12:34:12 -0400

    Added Missing Files

--- evaluate_NN.py
+++ evaluate_NN.py
@@ -1,42 +1,22 @@
 from create_NN import create_NN
 import torch
 from torch.utils.data import DataLoader, TensorDataset
-<<<<<<< HEAD
-=======
-from string import punctuation
->>>>>>> 8cbc7e1e0fd702f31dabc0d5bbdc995af9b7ead3
 import numpy as np
 import nltk
 import json
 
-<<<<<<< HEAD
 
 def evaluate_NN(all_text, dictFileName="dictionaryIMDB.json", seqLen=40):
     train_on_gpu = torch.cuda.is_available()
     device = torch.device('cuda:0' if train_on_gpu else 'cpu')
     vocabToInt = json.load(open(dictFileName, 'r'))  # load the dictionary
     all_text = all_text.lower()
-=======
-def evaluate_NN(all_text, dictFileName="dictionaryIMDB.json", seqLen=40):
-    train_on_gpu = torch.cuda.is_available()
-    device = torch.device('cuda:0' if train_on_gpu else 'cpu')
-
-    vocabToInt = json.load(open(dictFileName, 'r'))  # load the dictionary
-    all_text = all_text.lower()
-    all_text = ''.join(c for c in all_text if c not in punctuation)
->>>>>>> 8cbc7e1e0fd702f31dabc0d5bbdc995af9b7ead3
     words = all_text.split()  # make an array of words
     textListNum = list()  # to become list of lists holding tweets and words
     r1 = list()
     for word in words:  # iterate through text tweet by tweet
-<<<<<<< HEAD
         r = vocabToInt[word]  # convert words to unique integers
         r1.append(r)
-=======
-        if word in vocabToInt:
-            r = vocabToInt[word]  # convert words to unique integers
-            r1.append(r)
->>>>>>> 8cbc7e1e0fd702f31dabc0d5bbdc995af9b7ead3
     textListNum.append(r1)
 
     while len(textListNum[0]) < seqLen:  # force to uniform length
@@ -44,10 +24,6 @@ def evaluate_NN(all_text, dictFileName="dictionaryIMDB.json", seqLen=40):
         textListNum[0].append(np.random.randint(0, len(vocabToInt)))
     if len(textListNum[0]) > seqLen:
         textListNum[0] = textListNum[0][:seqLen]
-<<<<<<< HEAD
-=======
-        print(textListNum)
->>>>>>> 8cbc7e1e0fd702f31dabc0d5bbdc995af9b7ead3
     inputs = torch.tensor(np.array(textListNum), device=device)
     # load the model
     device = torch.device(device)
@@ -63,16 +39,9 @@ def evaluate_NN(all_text, dictFileName="dictionaryIMDB.json", seqLen=40):
     # output = net.test_model(1, inputs)
     net.eval()
     h = net.init_hidden(1, train_on_gpu)
-<<<<<<< HEAD
     h = tuple([each.data for each in h])
     output = net(inputs, h)
     print(output)
-=======
-
-    h = tuple([each.data for each in h])
-    output = net(inputs, h)
-    # print(output)
->>>>>>> 8cbc7e1e0fd702f31dabc0d5bbdc995af9b7ead3
     output = output[0]
     if train_on_gpu:
         output = output.cpu().detach().numpy()[0]
@@ -82,10 +51,5 @@ def evaluate_NN(all_text, dictFileName="dictionaryIMDB.json", seqLen=40):
     return output
 
 
-<<<<<<< HEAD
 evaluate_NN("amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing")
-=======
-# evaluate_NN("amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing amazing")
-
->>>>>>> 8cbc7e1e0fd702f31dabc0d5bbdc995af9b7ead3
 
---
+++ process_data.py
@@ -0,0 +1,121 @@
+import numpy as np
+from string import punctuation
+from collections import Counter
+from create_NN import create_NN
+import json
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+
+train_on_gpu = True
+
+# ### THIS LINE DEFINES A GPU, OTHERWISE CPU IS DEFAULT, UNCOMMENT IF YOU HAVE A GPU ###
+
+train_on_gpu = torch.cuda.is_available()
+# train_on_gpu = False
+device = torch.device('cuda:0' if train_on_gpu else 'cpu')
+
+print(device)
+
+textList = list()
+senList = list()
+with open("output_data_shuffled.txt", "r") as f:
+    lines = f.readlines()
+    i = 0
+    for line in lines:
+        if line.__contains__(","):
+            sentiment, text = line.split(",", 1)
+            text = ''.join([c for c in text if c not in punctuation])
+            text = text.lower()
+            while len(text) < 140:
+                text = text + " "
+            textList.append(text)
+            # senList.append((ord(sentiment[1]) - 48)/5.0)
+            if (ord(sentiment[1]) - 48) > 0:
+                senList.append(1)
+            else:
+                senList.append(0)
+            # if i == 200000:
+            #     break
+print("Finished reading file")
+
+# load existing word dict
+dictFileName = "dictionaryIMDB.json"
+vocabToInt = json.load(open(dictFileName, 'r'))  # load the dictionary
+print("Finished loading dictionary")
+# add new word dict
+# all_text = ' '.join(textList)  # this should create 1 string holding all tweets
+# words = all_text.split()
+# count_words = Counter(words)
+# print(count_words)
+# total_words = len(words)
+# sorted_words = count_words.most_common(total_words)
+# vocabToInt2 = {w: i+1 for i, (w, c) in enumerate(sorted_words)}  # create a dictionary mapping the individual words
+#                                                                 # to unique integers
+#
+# vocabToInt = vocabToInt1.copy()
+# vocabToInt.update(vocabToInt2)
+
+textListNum = list()  # to become list of lists holding tweets and words
+seqLen = 0
+for text in textList:  # iterate through text tweet by tweet
+    r = [vocabToInt[w] for w in text.split()]  # convert words to unique integers
+    if len(r) > seqLen:  # set seqLen to be the max length of a tweet (in words)
+        seqLen = len(r)
+    textListNum.append(r)
+print("seqLen = ")
+print(seqLen)
+inputs = np.zeros((len(textListNum), seqLen), dtype=int)  # 2D array formatting to suit NN batch input
+i = 0
+for nums in textListNum:
+    while len(nums) < seqLen:
+        nums.append(0)  # pad tweets with less words with zeros to match seqLen
+    inputs[i, :] = np.array(nums)
+    i = i+1
+
+outputs = np.array(senList)
+# create new model
+# net = create_NN(len(vocabToInt)+1, device)  # +1 for the 0 padding not yet in dictionary
+# load existing model
+net = create_NN(len(vocabToInt) + 1, device)
+# net.load_state_dict(torch.load("model.pt"))
+print("Created NN")
+if train_on_gpu:
+    net = net.cuda(device)  # ###  CONVERSION_TO_GPU_FORMAT
+    print("Transferred to GPU")
+print(next(net.parameters()).is_cuda)
+print("my net")
+print(net)  # ### IF GPU FORMAT, SHOULD STATE SO AT THE END OF THE PRINT STATEMENT
+
+length = len(inputs)
+split1 = 0.2
+split2 = 0.21
+# train_x = inputs[400000:int(split1*length)]
+# train_y = outputs[400000:int(split1*length)]
+
+train_x = inputs[0:int(split1*length)]
+train_y = outputs[0:int(split1*length)]
+valid_x = inputs[int(split1*length):int(split2*length)]
+print(len(valid_x))
+
+valid_y = outputs[int(split1*length):int(split2*length)]
+# test_x = inputs[int(split2*length):]
+# test_y = outputs[int(split2*length):]
+
+# create Tensor datasets
+train_data = TensorDataset(torch.from_numpy(train_x), torch.from_numpy(train_y))
+valid_data = TensorDataset(torch.from_numpy(valid_x), torch.from_numpy(valid_y))
+# test_data = TensorDataset(torch.from_numpy(test_x), torch.from_numpy(test_y))
+
+# dataloaders
+batch_size = 50
+train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size, drop_last=True)
+valid_loader = DataLoader(valid_data, shuffle=True, batch_size=batch_size, drop_last=True)
+# test_loader = DataLoader(test_data, shuffle=True, batch_size=batch_size)
+
+hidden = net.init_hidden(batch_size)
+net.train_model(batch_size, train_loader, valid_loader)
+# net.test_model(batch_size, test_loader)
+print(net)
+
+json.dump(vocabToInt, open("dictionaryBinary.json", 'w'))
+torch.save(net.state_dict(), "model2.pt")
