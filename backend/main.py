46dc00a3c34d4715ea25856c8b6dcd90daab9c7d
steveZak <chesl97@gmail.com>
2019-07-18 16:12:38 -0400

    Some tab switching action

--- backend/main.py
+++ backend/main.py
@@ -1,33 +1,43 @@
 # This function will handle all calls and passing of information
 # =============================================================================
-# import Web2String
-# import WebCrawler
-# import company_identifier
-# import StockTest
-# import evaluate_NN
+import Web2String
+import WebCrawler
+import company_identifier
+import StockTest
+import evaluate_NN
 # =============================================================================
 import json
+
+
 def main(url):
-# =============================================================================
-#     string = Web2String.url2string(url)
-#     entity = company_identifier.entity(string)
-#     # ticker = company_identifier.find_ticker(entity)
-#     other_articles = WebCrawler.getArticles(entity) # array of article urls
-#     StockTest.stockGraph(entity)
-#     
-#     # print(string)
-#     main_article_sentiment = evaluate_NN.evaluate_NN(string)  # analyze sentiment of main article
+    string = Web2String.url2string(url)
+    print(string)
+    entity = company_identifier.entity(string)
+    print(entity)
+    # ticker = company_identifier.find_ticker(entity)
+    other_articles = WebCrawler.getArticles(entity)  # array of article urls
+    other_articles = list()
+    other_articles.append("https://www.reuters.com/article/us-apple-services-research/apples-services-revenue-china-to-power-third-quarter-analyst-idUSKCN1U40SH")
+    other_articles.append("https://www.reuters.com/article/us-netflix-stocks/netflix-sinks-on-subscriber-losses-analysts-still-see-growth-idUSKCN1UD1SL")
+    other_articles.append("https://www.reuters.com/article/us-usa-stocks/wall-street-falls-as-netflix-adds-to-earnings-jitters-idUSKCN1UD1PD")
+    other_articles.append("https://www.theverge.com/2019/7/18/20699037/netflix-earnings-report-q2-streaming-wars-disney-apple-warnermedia-international")
+    other_articles.append("https://www.cnbc.com/2019/07/17/netflix-earnings-q2-2019.html")
+
+    print(other_articles)
+    StockTest.stockGraph(entity)
+    # print(string)
+    main_article_sentiment = round(evaluate_NN.evaluate_NN(string)*100)  # analyze sentiment of main article
+    print(main_article_sentiment)
+    # arrays that will hold similar/different articles and their sentiments
+    other_article_sentiment = list()
 # 
-#     # arrays that will hold similar/different articles and their sentiments
-#     other_article_sentiment = []
-# 
-#     for x in range(len(other_articles)):
-#         curr_article = Web2String.url2string(other_articles[x])
-# 
-#         # analyze sentiment of article and put in array
-#         other_article_sentiment[x] = evaluate_NN.evaluate_NN(curr_article)
-#         # other_article_sentiment.append(evaluate_NN.evaluate_NN(curr_article))
-#     # print(entity)
+    for x in range(len(other_articles)):
+        curr_article = Web2String.url2string(other_articles[x])
+
+        # analyze sentiment of article and put in array
+        # other_article_sentiment[x] = round(evaluate_NN.evaluate_NN(curr_article)*100)
+        other_article_sentiment.append(round(evaluate_NN.evaluate_NN(curr_article)*100))
+    # print(entity)
 #     return main_article_sentiment*10
 # 
 # =============================================================================
@@ -39,10 +49,14 @@ def main(url):
 #     entity = "Deutsche Bank"
 # =============================================================================
     
-    other_articles_dict = { 'other_articles' : { 'one' : 'sampleOne.com', 'two' : 'sampleTwo.com', 'three' : 'sampleThree.com', 'four' : 'sampleFour.com', 'five' : 'sampleFive.com' }}
-    other_article_sentiment_dict = { 'other_articles_sentiment' : { 'one' : 1, 'two' : 2, 'three' : 3, 'four' : 4, 'five' : 5 }}
-    main_article_sentiment_dict = { 'main_article_sentiment' : 10 }
-    entity_dict = { 'company_name' : 'Deutsche Bank' }
+    other_articles_dict = {'other_articles': {'one': 'sampleOne.com', 'two': 'sampleTwo.com', 'three': 'sampleThree.com', 'four': 'sampleFour.com', 'five': 'sampleFive.com'}}
+    other_article_sentiment_dict = {'other_articles_sentiment': {'one': other_article_sentiment[0],
+                                                                 'two': other_article_sentiment[1],
+                                                                 'three': other_article_sentiment[2],
+                                                                 'four': other_article_sentiment[3],
+                                                                 'five': other_article_sentiment[4]}}
+    main_article_sentiment_dict = {'main_article_sentiment': main_article_sentiment}
+    entity_dict = {'company_name': entity}
     
     rv = {}
     
--- frontend/js/popup.js
+++ frontend/js/popup.js
@@ -3,8 +3,50 @@
  });
   port.postMessage("starting up");
  port.onMessage.addListener(function(msg) {
+    document.getElementById("add-sent").style.display = "none"
+    document.getElementById("stock-graph").style.display = "none"
+    document.getElementById("sentiment-btn").addEventListener('click',
+        function(){
+            openTab(event, 'main-sent');
+        }, false)
+    document.getElementById("stock-btn").addEventListener('click',
+        function() {
+            openTab(event, 'add-sent');
+        }, false)
+    document.getElementById("article-btn").addEventListener('click',
+        function() {
+            openTab(event, 'stock-graph');
+        }, false)
     data = JSON.parse(msg);
-
-    document.getElementById("sent-circle").setAttribute("data-progress", "15");
-
+    entity_name = data.company_name;
+    main_sent = data.main_article_sentiment;
+    add_sent = data.other_articles_sentiment;
+    document.getElementById("entity-name").innerHTML = entity_name;
+    document.getElementById("sent-circle-main").setAttribute("data-progress", main_sent);
+    document.getElementById("sent-circle-add0").setAttribute("data-progress", add_sent.one);
+    document.getElementById("sent-circle-add1").setAttribute("data-progress", add_sent.two);
+    document.getElementById("sent-circle-add2").setAttribute("data-progress", add_sent.three);
+    document.getElementById("sent-circle-add3").setAttribute("data-progress", add_sent.four);
+    document.getElementById("sent-circle-add4").setAttribute("data-progress", add_sent.five);
  });
+
+function openTab(evt, eltName) {
+    // Declare all variables
+    var i, tabcontent, header;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("header");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(eltName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
