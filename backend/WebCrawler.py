0ef4c695faa41f3faceb5c930870c272c06c03b2
AlfredJoseph <ajosep16@uncc.edu>
2019-07-19 11:39:54 -0400

    Uses GoogleNews API

--- backend/WebCrawler.py
+++ backend/WebCrawler.py
@@ -1,9 +1,22 @@
-from GoogleNews import GoogleNews
+from newsapi import NewsApiClient
+
+def getInfo(query):
+    #Key to access GoogleNews API
+    newsapi = NewsApiClient(api_key='edf0afe93d6644d198d8539e640134c9')
+    #query
+    headlines = newsapi.get_top_headlines(q=query)
+    newsTitles = list()
+    newsContent = list()
+    newsSources = list()
+    newsURL = list()
+    #Adds all relevant information to separate lists
+    for x in range(5):
+        newsTitles.append(headlines['articles'][x]['title'])
+        newsContent.append(headlines['articles'][x]['content'])
+        newsSources.append(headlines['articles'][x]['source']['name'])
+        newsURL.append(headlines['articles'][x]['url'])
+    return newsTitles, newsContent, newsSources, newsURL
 
 
-# returns a list of 5 articles related to the query
-def getArticles(query):
-    googlenews = GoogleNews()
-    googlenews.search(query)
-    x = googlenews.getlinks()
-    return x
+
+
